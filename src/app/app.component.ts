c5089ca08138cb21c63d0b007ddf02597fcdc032
Limani Ratnayake <lpw.ratnayake@gmail.com>
2025-06-23 13:29:37 +0530

    change the base url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true, 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
